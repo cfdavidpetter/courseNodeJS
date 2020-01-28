b685f7e75c61c216463e9a425b744b2085583512
David Petter Alves <cfdavidpetter@gmail.com>
2020-01-28 07:53:35 -0300

    first commit

---
+++ src/config/database.js
@@ -0,0 +1,10 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: '123456',
+    ddefine: {
+        timestamps: true,
+        underscored: true,
+    },
+};
